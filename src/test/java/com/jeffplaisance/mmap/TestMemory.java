27c602a6b129b85c4dd594d76126039a2b999501
j <jeffplaisance@gmail.com>
2013-05-31 00:27:12 -0500

    super basic test

---
+++ src/test/java/com/jeffplaisance/mmap/TestMemory.java
@@ -0,0 +1,23 @@
+package com.jeffplaisance.mmap;
+
+import junit.framework.TestCase;
+
+import java.nio.ByteOrder;
+
+public class TestMemory extends TestCase {
+    public void testStuff() {
+        final long[] data = new long[8];
+        final HeapMemory heap = new HeapMemory(data, 0, 64, ByteOrder.nativeOrder());
+        for (int i = 0; i < data.length; i++) {
+            heap.putLong(i * 8, i);
+        }
+        for (int i = 0; i < data.length; i++) {
+            assertEquals(data[i], i);
+        }
+        DirectMemory direct = new DirectMemory(NativeMMapUtils.getUnsafe().allocateMemory(64), 64, ByteOrder.nativeOrder());
+        direct.putBytes(0, heap);
+        for (int i = 0; i < data.length; i++) {
+            assertEquals(data[i], direct.getLong(i*8));
+        }
+    }
+}
